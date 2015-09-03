7c641f7990a2c0fc9206016c22172e7f1febc305
brianm85 <mitcho85@hotmail.com>
2015-09-03 15:38:44 +0100

    huffman code for data compression
    Compress a sentance with huffman coding

---
+++ Node.java
@@ -0,0 +1,9 @@
+class Node
+   {
+ 
+   public char letter;            //stores letter
+   public int weight;
+   public Node leftChild;         // this node's left child
+   public Node rightChild;        // this node's right child
+
+}  // end class Node
---
+++ Tree.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;               // for Stack class
+
+////////////////////////////////////////////////////////////////
+public class Tree implements Comparable<Tree>
+   {
+   public Node root;             // first node of tree
+   public int frequency=0;
+
+// -------------------------------------------------------------
+   public Tree()                  // constructor
+      {   root = null; }            // no nodes in tree yet
+// -------------------------------------------------------------
+
+//the PriorityQueue needs to be able to somehow rank the objects in it
+//thus, the objects in the PriorityQueue must implement an interface called Comparable
+//the interface requires you to write a compareTo() method so here it is:
+
+   public int compareTo(Tree object){ //
+       if(frequency-object.frequency>0){ //compare the cumulative frequencies of the tree
+           return 1;
+        }else if(frequency-object.frequency<0){
+           return -1;   //return 1 or -1 depending on whether these frequencies are bigger or smaller
+        }else{
+            return 0;   //return 0 if they're the same
+        }
+   }
+    
+// -------------------------------------------------------------
+
+   String path="error";     //this variable will track the path to the letter we're looking for 
+
+   public String getCode(char letter){  //we want the code for this letter
+ 
+       //FILL THIS IN:
+       
+       //How do you get the code for the letter? Maybe try a traversal of the tree
+       //Track the path along the way and store the current path when you arrive at the right letter
+       
+       return path;     //return the path that results
+            
+   }
+
+}  // end class Tree
+///////////////////////////////
